919046ec2345fa9ffd3a0052691c201e47a7b3b5
JazC <jzchan7713@gmail.com>
2022-12-22 23:01:59 -0800

    Add responsive test table

--- client/src/LinkRow.js
+++ client/src/LinkRow.js
@@ -3,10 +3,14 @@ import React from 'react'
 function LinkRow({ buttonInfo }) {
   return (
     <div className='ReminderRowDiv'>
-      <div>
-        <a href='https://www.google.com'>https://www.google.com</a>
+      <div className='LinkDiv'>
+        {/* <a href='https://www.google.com'>https://www.google.commeqggewagewagewagewraegeagewewageewagewagewagewraegewagewagewraege</a> */}
+        {/* <a href='https://www.google.com'>https://www.google.commeqggewagewagewageaegewagewagewraege</a> */}
+        <a href='https://www.google.comm'>https://www.google.com</a>
+
+        {/* Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. */}
       </div>
-      <div>
+      <div className='ButtonInfoDiv'>
         <p>{buttonInfo}</p>
       </div>
     </div>
--- client/src/ReminderBottom.js
+++ client/src/ReminderBottom.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReminderHeading from './ReminderHeading'
 import LinkRow from './LinkRow'
+import TestTable from './TestTable'
 
 function ReminderBottom({ buttonInfo }) {
   return (
     <>
       <div className='ReminderBottomSubDiv'>
-        <ReminderHeading />
-        <LinkRow buttonInfo={buttonInfo} />
+      <TestTable />
+        {/* <ReminderHeading /> */}
+        
+        {/* <LinkRow buttonInfo={buttonInfo} />
+        <LinkRow buttonInfo={"buttonInfo"} /> */}
+        
       </div>
 
     </>
